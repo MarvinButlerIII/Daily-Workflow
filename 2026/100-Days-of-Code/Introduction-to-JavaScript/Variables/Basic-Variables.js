c2ac1fa3cfe7c64b26f27fa9d620a19d133d3661
MarvinButlerIII <butlermarviniii@gmail.com>
2026-01-13 16:12:29 -0600

    Marvin Butler III

--- 2026/100-Days-of-Code/Introduction-to-JavaScript/Variables/Basic-Variables.js
+++
@@ -1,8 +0,0 @@
-var petDog = "Rex";
-var dogSound = "woof"
-console.log(petDog, "says", dogSound);
-
-var petCat = "Pepper"
-var catSound = "purr";
-var catSound = "meow";
-console.log(petCat, "now says", catSound);
